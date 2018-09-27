ad74d2ef84a1c00b7dddc3277dcb19b45389dfeb
gtatoy <gavinvtatoy@su.edu.ph>
2018-09-27 11:03:53 +0800

    changes to files

--- login.php
+++ login.php
@@ -50,16 +50,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             save the username to the session */
                             session_start();
                             $_SESSION['username'] = $username;
-                            // $_SESSION['accountID']=;
-                            header("location: menu_burger.php");
+                            header("location: signup.php");
                         } else{
                             // Display an error message if password is not valid
-                            $password_err = 'The password you entered was not valid.';
+                            $password_err = 'Invalid password';
                         }
                     }
                 } else{
                     // Display an error message if username doesn't exist
-                    $username_err = 'No account found with that username.';
+                    $username_err = 'Username not found';
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
@@ -76,99 +75,46 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
+
 <head>
   <meta charset="UTF-8">
-  <title>Mcdelivery</title>
-  <link rel="stylesheet" type="text/css" href="style2.css" >
-  <link rel="stylesheet" type="text/css" href="styles.css" >
-  <link rel="shortcut icon" href="mc-logo.png">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <style type="text/css">
-      body{
-        font: 14px sans-serif; }
-        .wrapper{
-          position: relative;
-          margin-left: 125%;
-          margin-top: 50%;
-          width: 300px;
-          height: 300px;
-          padding-left: 60px;
-          padding-top: 30px;
-          }
-  </style>
+  <title>McDelivery Login</title>
+  
+     <link rel="stylesheet" href="css/style.css">
 </head>
 
-<header class ="BackgroundColor">
-    <div class="row">
-      <div class="logo">
-        <a href="menu.html"><img src="logo.png" alt="logo"></a>
-      </div>
-    </div>
+<body>
 
-</header>
+  <div class="wrapper">
+	<div class="container">
+		<img src="logo2.png" alt="logo">
+		<h1>Welcome</h1>
+		
+		<form class="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+			<input type="text" name="username" placeholder="<?php echo (!empty($username_err)) ? $username_err : 'Enter Username'; ?>">
+			<input type="password" name="password" placeholder="<?php echo (!empty($password_err)) ? $password_err : 'Enter Password'; ?>">
+			<!-- <input type="password" placeholder="Re-enter Password"> -->
+            <button type="submit" id="login-button">Login</button>
+            <p><br>Don't have an account? <a href="signup.php" id="loginlink">Sign up now</a>.</p>
+		</form>
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
 </div>
-
-
-<body  class="loginsystem">
-
-
-    <div class="wrapper">
-        <h2 id="signin">Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-          <br>
-          <div class = "usernamelogin">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
-                <span class="help-block"><?php echo '<br>'.$username_err; ?></span>
-            </div>
-          </div>
-          <br>
-          <div class = "passwordlogin">
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo '<br>'.$password_err; ?></span>
-            </div>
-          </div>
-          <br>
-          <div class= "loginbutton1">
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-          </div>
-          <br>
-          <br>
-          <br>
-            <p>Don't have an account? <a href="registerAccount.php" id="loginlink">Sign up now</a>.</p>
-        </form>
-  </div>
-
-
-
-    <div class="social_media">
-      <div><a target="_blank" rel="noopener" href="https://www.facebook.com/McDo.ph/"><i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i></a></div>
-      <div><a target="_blank" rel="noopener" href="https://instagram.com/mcdo_ph/?hl=en"><i class="fa fa-instagram fa-3x"></i></a></div>
-      <div><a target="_blank" rel="noopener" href="https://twitter.com/mcdo_ph"><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a></div>
-    </div>
-    <div class="footer">
-      <div><a href="">ABOUT</a></div>
-      <div><a href="">CHARITY</a></div>
-      <div><a href="">CAREERS</a></div>
-      <div><a href="">PRESS CENTRE</a></div>
-      <div><a href="">GALLERY</a></div>
-      <div><a href="">BUSINESS OPPORTUNITY</a></div>
-      <div><a href="">CUSTOMER CARE</a></div>
-      <div><a href="">NEWSLETTER</a></div>
-      <div><a href="">TERMS AND CONDITION</a></div>
-      <div><a href="">PRIVACY POLICY</a></div>
-    </div>
-
-
-
-
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+	<!-- <script src="js/index.js"></script> -->
 </body>
+
 </html>
